fbbec2de36b3a820cbf9af7bee50027524387b1f
jagger1117 <143247664+jagger1117@users.noreply.github.com>
2023-11-18 18:54:38 -0500

    Add files via upload

---
+++ Figuras/src/figuras/Figuras.java
@@ -0,0 +1,10 @@
+package figuras;
+
+public class Figuras {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(false);
+    }
+}
---
+++ notas/src/notas/VentanaPrincipal.java
@@ -0,0 +1,129 @@
+package notas;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class VentanaPrincipal extends JFrame implements ActionListener {
+    private Container contenedor;
+    private JLabel nota1, nota2, nota3, nota4, nota5, promedio, desviación, mayor, menor;
+    private JTextField campoNota1, campoNota2, campoNota3, campoNota4, campoNota5;
+    private JButton calcular, limpiar;
+
+    public VentanaPrincipal() {
+        inicio();
+        setTitle("Notas");
+        setSize(280, 380);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+
+    private void inicio() {
+        contenedor = getContentPane();
+        contenedor.setLayout(null);
+
+        nota1 = new JLabel();
+        nota1.setText("Nota 1:");
+        nota1.setBounds(20, 20, 150, 23);
+        campoNota1 = new JTextField();
+        campoNota1.setBounds(125, 20, 150, 23);
+
+        nota2 = new JLabel();
+        nota2.setText("Nota 2:");
+        nota2.setBounds(20, 50, 150, 23);
+        campoNota2 = new JTextField();
+        campoNota2.setBounds(125, 50, 150, 23);
+
+        nota3 = new JLabel();
+        nota3.setText("Nota 3:");
+        nota3.setBounds(20, 80, 150, 23);
+        campoNota3 = new JTextField();
+        campoNota3.setBounds(125, 80, 150, 23);
+
+        nota4 = new JLabel();
+        nota4.setText("Nota 4:");
+        nota4.setBounds(20, 110, 150, 23);
+        campoNota4 = new JTextField();
+        campoNota4.setBounds(125, 110, 150, 23);
+
+        nota5 = new JLabel();
+        nota5.setText("Nota 5:");
+        nota5.setBounds(20, 140, 150, 23);
+        campoNota5 = new JTextField();
+        campoNota5.setBounds(125, 140, 150, 23);
+
+        calcular = new JButton();
+        calcular.setText("Calcular");
+        calcular.setBounds(20, 180, 120, 23);
+
+        calcular.addActionListener(this);
+
+        limpiar = new JButton();
+        limpiar.setText("Limpiar");
+        limpiar.setBounds(150, 180, 100, 23);
+
+        limpiar.addActionListener(this);
+
+        promedio = new JLabel();
+        promedio.setText("Promedio = ");
+        promedio.setBounds(20, 220, 150, 23);
+
+        desviación = new JLabel();
+        desviación.setText("Desviación = ");
+        desviación.setBounds(20, 250, 200, 23);
+
+        mayor = new JLabel();
+        mayor.setText("Nota mayor = ");
+        mayor.setBounds(20, 280, 120, 23);
+
+        menor = new JLabel();
+        menor.setText("Nota menor = ");
+        menor.setBounds(20, 310, 120, 23);
+
+        contenedor.add(nota1);
+        contenedor.add(campoNota1);
+        contenedor.add(nota2);
+        contenedor.add(campoNota2);
+        contenedor.add(nota3);
+        contenedor.add(campoNota3);
+        contenedor.add(nota4);
+        contenedor.add(campoNota4);
+        contenedor.add(nota5);
+        contenedor.add(campoNota5);
+        contenedor.add(calcular);
+        contenedor.add(limpiar);
+        contenedor.add(promedio);
+        contenedor.add(desviación);
+        contenedor.add(mayor);
+        contenedor.add(menor);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+        if (evento.getSource() == calcular) {
+            Notas notas = new Notas();
+            notas.listaNotas[0] = Double.parseDouble(campoNota1.getText());
+            notas.listaNotas[1] = Double.parseDouble(campoNota2.getText());
+            notas.listaNotas[2] = Double.parseDouble(campoNota3.getText());
+            notas.listaNotas[3] = Double.parseDouble(campoNota4.getText());
+            notas.listaNotas[4] = Double.parseDouble(campoNota5.getText());
+            notas.calcularPromedio();
+            notas.calcularDesviación();
+            promedio.setText("Promedio = " + String.valueOf(String.format("%.2f", notas.calcularPromedio())));
+            double desv = notas.calcularDesviación();
+            desviación.setText("Desviación estándar = " + String.format("%.2f", desv));
+            mayor.setText("Valor mayor = " + String.valueOf(notas.calcularMayor()));
+            menor.setText("Valor menor = " + String.valueOf(notas.calcularMenor()));
+        }
+
+        if (evento.getSource() == limpiar) {
+            campoNota1.setText("");
+            campoNota2.setText("");
+            campoNota3.setText("");
+            campoNota4.setText("");
+            campoNota5.setText("");
+        }
+    }
+}
+
